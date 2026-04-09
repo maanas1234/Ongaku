c654d719b3070bd3b5403a531c43c865dffa0211
maanas1234 <maanastyagi2006@gmail.com>
2026-04-09 15:34:56 +0530

    made the first draft

---
+++ app.py
@@ -0,0 +1,52 @@
+from langgraph.graph import StateGraph, END
+from typing import TypedDict
+from LLM import get_llm
+from prompts import song_picker,genre_decide,song_recommend
+
+
+llm = get_llm()
+
+class Mystate(TypedDict):
+    sentence : str
+    mood : str
+    genre: str
+    song : str
+
+def node_1(state:Mystate) -> dict:
+    song_picker_temp= song_picker.format(sentence = state['sentence'])
+    response = llm.invoke(song_picker_temp)
+    state['mood']=response
+    return {}
+
+def node_2(state:Mystate) -> dict:
+    genre_decide_temp= genre_decide.format(mood=state['mood'],sentence=state['sentence'])
+    response = llm.invoke(genre_decide_temp)
+    state['genre'] = response.content
+    return {}
+
+def node_3(state:Mystate) -> dict:
+    song_recommend_t = song_recommend.format(genre = state['genre'],mood = state['mood'])
+    final_out = llm.invoke(song_recommend_t)
+    state['song']= final_out.content
+    return {}
+
+
+
+graph = StateGraph(Mystate)
+
+graph.add_node("node_one", node_1)
+graph.add_node("node_two", node_2)
+graph.add_node("node_three",node_3)
+
+graph.add_edge("node_one", "node_two")
+graph.add_edge("node_two", "node_three")
+graph.add_edge("node_three",END)
+
+graph.set_entry_point("node_one")
+
+app=graph.compile()
+result = app.invoke({"sentence":"My girlfriend and I talked on VC for 1hr last night. It was fun.","mood":"","genre":"","song":""})
+
+print(f"final: {result}")
+
+
--- prompts.py
+++ prompts.py
@@ -1,4 +1,10 @@
 from langchain_core.prompts import PromptTemplate
 
-Song_picker_template = """You need to analyse the user's mood based on the given sentence "{mood}", and suggest the user some songs to listen to uplift that mood."""
+Song_picker_template = """You need to analyse the user's mood based on the given sentence "{sentence}". Only reply with one word that is the mood, nothing else."""
 song_picker = PromptTemplate.from_template(template = Song_picker_template)
+
+Genre_decide_template = """You need to find the genre for a song for the given mood "{mood}" The original user statement was this {sentence}, from which the mood is deduced. Reply with only 1 word which is the genre and nothing else."""
+genre_decide = PromptTemplate.from_template(template = Genre_decide_template)
+
+Song_recommend_temp = """You need to recommend the best song based on the genre which is{genre}. The user whom you are recommending this to has the mood {mood}"""
+song_recommend = PromptTemplate.from_template(template = Song_recommend_temp)
